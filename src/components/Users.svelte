af2c5beac992f1dc19363ce541212babfc2db725
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-09-15 12:11:59 +0200

    Setup svelte

---
+++ src/components/Users.svelte
@@ -0,0 +1,35 @@
+<script>
+import { onMount } from "svelte";
+
+
+export let users = [{name: "janne"}, {name: "jenni"}]
+
+onMount(() => {
+    const fetchUsers = async () => {
+        let returnData = await fetch('http://localhost:3000/users').then(res => res.json()).then(data => data)
+        return returnData
+    }
+
+    users = fetchUsers()
+    console.log(users)
+})
+</script>
+
+<style>
+
+</style>
+
+<div>
+    {#each users as user}
+
+    <div>
+        {user.name}
+    </div>
+
+    {:else}
+     <div>
+         Hämtar användare...
+     </div>
+    {/each}
+
+</div>
