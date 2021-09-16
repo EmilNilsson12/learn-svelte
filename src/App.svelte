cdac5a6a6b953a513e59146443d421165adf9200
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-09-16 14:09:56 +0200

    Test eventDispatcher

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,9 @@
 <script>
-    // Alert
-    function handleClick({ target }) {
-        alert('clicked: ' + target.id);
+    import Inner from './Inner.svelte';
+
+    function handleMessage(event) {
+        alert(event.detail.text);
     }
 </script>
 
-<div>
-    <h3>With stopPropagation</h3>
-    <button id="outer btn" on:click={handleClick}>
-        Outer button<button
-            id="inner btn"
-            on:click|stopPropagation={handleClick}
-            >Inner button, click to test stopPropagation</button
-        >
-    </button>
-</div>
-
-<div>
-    <h3>Without stopPropagation</h3>
-    <button id="outer btn" on:click={handleClick}>
-        Outer button<button id="inner btn" on:click={handleClick}
-            >Inner button, click to test stopPropagation</button
-        >
-    </button>
-</div>
+<Inner on:message={handleMessage} />
